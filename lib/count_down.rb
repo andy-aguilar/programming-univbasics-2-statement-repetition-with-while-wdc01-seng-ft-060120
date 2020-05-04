fb6e689f16b8147cdf9ca741a983c0a2ead839a4
Andres Aguilar <aaguil3@gmail.com>
2020-05-04 15:47:54 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,12 +1,7 @@
 # Write your code here
 count = 10
-magic_exit_number = 0 
-while count > -1 do
-  break if count == magic_exit_number
+while count >= 1 do
   puts count
   count -= 1 
 end
-
-if count == magic_exit_number
-  puts "Happy New Year!"
-end
+puts "Happy New Year!"
